7352c9ea785face3feed739ae9279cd602cf5192
Martin Polak <martinpolak@MacBook-Pro-von-Martin.local>
2024-11-07 23:39:11 +0100

    first controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index()
+    {
+        return New Response('Hello Symfony World!');
+    }
+}
